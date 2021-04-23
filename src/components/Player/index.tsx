e56478e6d694b8ad01f5e623c0c6daad17ec4010
Antonio-HO <antonio.aroucha@armateus.com.br>
2021-04-23 11:10:10 -0300

    exibir dados no player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,27 +1,40 @@
+import Image from "next/image";
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
 
 export function Player() {
-
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
-  const episode = episodeList[currentEpisodeIndex]
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+  const episode = episodeList[currentEpisodeIndex];
 
   return (
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando agora" />
-        <strong>Tocando agora { episode?.title }</strong>
+        <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
       <footer className={styles.empty}>
         <div className={styles.progress}>
           <span>00:00</span>
-          <div className={styles.slider} >
+          <div className={styles.slider}>
             <div className={styles.emptySlider} />
           </div>
           <span>00:00</span>
