49ffa26398360462adcf60eb5dff51adc3aa8a70
Antonio-HO <antonio.aroucha@armateus.com.br>
2021-04-23 10:28:58 -0300

    adicionar contexto a aplicacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,19 @@ import "../styles/global.scss";
 
 import styles from "../styles/app.module.scss";
 import { Player } from "../components/Player";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={"Junior"}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
