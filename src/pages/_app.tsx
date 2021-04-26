6f72ecb6226984b6e7c450df772ebe1a5ee73923
Antonio-HO <antonio.aroucha@armateus.com.br>
2021-04-26 15:39:48 -0300

    organizar contexto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,32 +2,17 @@ import { Header } from "../components/Header";
 
 import styles from "../styles/app.module.scss";
 import { Player } from "../components/Player";
-import { PlayerContext } from "../contexts/PlayerContext";
+import {
+  PlayerContext,
+  PlayerContextProvider,
+} from "../contexts/PlayerContext";
 import { useState } from "react";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state)
-  }
   return (
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, isPlaying, play, togglePlay, setPlayingState}}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -35,7 +20,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
